b832f7c76de06d02b56a0ea6b4efc67a78f16efd
LubianCombita <puertadiego@gmail.com>
2019-08-08 23:06:37 -0500

    Create parametro of conection for database. Create Model "Owner". Not start project for create database.

---
+++ MyLeasing.Web/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyLeasing.Web.Data.Entities;
+
+//To register the models of database, and generate conection' context
+namespace MyLeasing.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Owner> Owners { get; set; }
+    }
+}
